9219a45b8417b2db5d1115c9560ff48428b3f2bd
Sam Lewis <Samuel-Lewis@users.noreply.github.com>
2015-09-29 12:18:00 +0930

    Changed to more general path
    Also commented showing that this is an OSX implementation only

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-cp -f "Assistant.py" "/Users/Jacob/Library/Application Support/Sublime Text 3/Packages/User/Assistant.py"
+#OSX installation
+cp -f "Assistant.py" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Assistant.py"
